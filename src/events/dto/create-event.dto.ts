23873196d8a0d0649532799085ecc7b4c9d9e56b
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-14 12:25:28 +0700

    fix create dto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,22 +1,14 @@
 import { IsNotEmpty, MinLength } from "class-validator";
 
 export class CreateEventDto {
+ 
     id?: string;
-    @IsNotEmpty()
-    @MinLength(3)
-    title: string;
-    @IsNotEmpty()
-    startDate: Date;
-    @IsNotEmpty()
-    endDate: Date;
-    @IsNotEmpty()
-    display: string;
-    @IsNotEmpty()
-    color: string;
-    @IsNotEmpty()
-    email:string;
-    @IsNotEmpty()
-    idCalender: string;
-    @IsNotEmpty()
+    title?: string;
+    startDate?: Date;
+    endDate?: Date;
+    display?: string;
+    color?: string;
+    email?:string;
+    idCalender?: string;
     freeStatus:boolean;
 }
