b9f40768597afb153b8ef86a421a090c01fc0c14
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-20 15:49:48 +0700

    add id

--- src/auth/utils/googleStrategy.ts
+++ src/auth/utils/googleStrategy.ts
@@ -10,8 +10,8 @@ export class googleStrategy extends PassportStrategy(Strategy, 'google') {
   ) {
     super({
       clientID:
-        '593588518556-laoh0qf29n6kr4ag2835t62ruspm6mom.apps.googleusercontent.com',
-      clientSecret: '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',
+        process.env.CLIENTID,
+      clientSecret: process.env.CLIENTSECRET,
       callbackURL: 'http://localhost:3000/api/auth/google/redirect',
       scope: ['profile', 'email'],
     });
