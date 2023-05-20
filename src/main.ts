62e2e6714b36551649cccc15752998ea64039b35
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-20 17:38:27 +0700

    change port test

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
  
-  await app.listen( process.env.PORT  );
+  await app.listen( 'https://rufreebackweb-production.up.railway.app' );
 }
 bootstrap();
